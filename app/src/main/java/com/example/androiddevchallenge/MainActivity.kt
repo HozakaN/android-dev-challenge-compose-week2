fa52d72e52779c14da28934f21ddf62d3fe4d65b
Guillaume Imbert <guillaume.m.imbert@gmail.com>
2021-03-04 07:37:53 +0100

    Animated countdown

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -22,37 +22,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.core.*
 import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.verticalDrag
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.input.pointer.util.VelocityTracker
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.theme.Numbers
 import com.example.androiddevchallenge.ui.theme.PathNumber
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlin.math.absoluteValue
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.roundToInt
@@ -62,8 +58,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme(darkTheme = true) {
-//                MyApp()
-                Gesture()
+                MyApp()
+//                Gesture()
             }
         }
     }
@@ -72,65 +68,81 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    val offset = remember { Animatable(Offset(0f, 0f), Offset.VectorConverter) }
-//    Surface(color = MaterialTheme.colors.background) {
-//        Text(text = "Ready... Set... GO!")
-//    }
-    ConstraintLayout(
-        modifier = Modifier
-            .fillMaxSize()
-            .pointerInput(Unit) {
-                coroutineScope {
-                    while (true) {
-                        // Detect a tap event and obtain its position.
-                        val position = awaitPointerEventScope {
-                            awaitFirstDown().position
-                        }
-                        launch {
-                            // Animate to the tap position.
-                            "animating".hozLog()
-                            offset.animateTo(position)
-                        }
-                    }
-                }
-            }
-    ) {
 
-        val (startButton, swipeLayout, myBox) = createRefs()
+    var displayedNumber by remember { mutableStateOf(Numbers.Zero) }
+    val transition = updateTransition(targetState = displayedNumber)
 
-        Box(modifier = Modifier
-            .constrainAs(myBox) {
-                top.linkTo(parent.top, 16.dp)
-                start.linkTo(parent.start, 16.dp)
-            }
-            .background(color = Color.Blue)
-            .requiredSize(100.dp)
-            .offset { offset.value.toIntOffset() }
-            /*.padding(start = 16.dp, top = 16.dp)*//*.test()*/)
-
-        CountdownView(
-            modifier = Modifier
-                .constrainAs(startButton) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                }/*.swipeToDismiss()*/
-        )
-
-        Image(
-            painter = painterResource(id = R.drawable.ic_android_black_24dp),
-            contentDescription = "",
+    Box(modifier = Modifier.fillMaxSize()) {
+        DrawNumber(
+            pathNumber = displayedNumber,
+            transition = transition,
             modifier = Modifier
                 .requiredSize(100.dp)
-                .constrainAs(swipeLayout) {
-                    top.linkTo(parent.top, 16.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-//                .swipeToDismiss()
+                .clickable { displayedNumber++ }
         )
     }
+
+
+
+//    val offset = remember { Animatable(Offset(0f, 0f), Offset.VectorConverter) }
+////    Surface(color = MaterialTheme.colors.background) {
+////        Text(text = "Ready... Set... GO!")
+////    }
+//    ConstraintLayout(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .pointerInput(Unit) {
+//                coroutineScope {
+//                    while (true) {
+//                        // Detect a tap event and obtain its position.
+//                        val position = awaitPointerEventScope {
+//                            awaitFirstDown().position
+//                        }
+//                        launch {
+//                            // Animate to the tap position.
+//                            "animating".hozLog()
+//                            offset.animateTo(position)
+//                        }
+//                    }
+//                }
+//            }
+//    ) {
+//
+//        val (startButton, swipeLayout, myBox) = createRefs()
+//
+//        Box(modifier = Modifier
+//            .constrainAs(myBox) {
+//                top.linkTo(parent.top, 16.dp)
+//                start.linkTo(parent.start, 16.dp)
+//            }
+//            .background(color = Color.Blue)
+//            .requiredSize(100.dp)
+//            .offset { offset.value.toIntOffset() }
+//            /*.padding(start = 16.dp, top = 16.dp)*//*.test()*/)
+//
+//        CountdownView(
+//            modifier = Modifier
+//                .constrainAs(startButton) {
+//                    top.linkTo(parent.top)
+//                    start.linkTo(parent.start)
+//                    end.linkTo(parent.end)
+//                    bottom.linkTo(parent.bottom)
+//                }/*.swipeToDismiss()*/
+//        )
+//
+//        Image(
+//            painter = painterResource(id = R.drawable.ic_android_black_24dp),
+//            contentDescription = "",
+//            modifier = Modifier
+//                .requiredSize(100.dp)
+//                .constrainAs(swipeLayout) {
+//                    top.linkTo(parent.top, 16.dp)
+//                    start.linkTo(parent.start)
+//                    end.linkTo(parent.end)
+//                }
+////                .swipeToDismiss()
+//        )
+//    }
 }
 
 @Composable
@@ -209,82 +221,31 @@ fun PreviewDrawOne() {
 }
 
 @Composable
-fun TestComposable(modifier: Modifier = Modifier) {
-    Box(modifier = modifier) {
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Green)
-        ) {
-            val canvasWidth = size.width
-            val canvasHeight = size.height
-
-            drawLine(
-                start = Offset(x = canvasWidth, y = 0f),
-                end = Offset(x = 0f, y = canvasHeight),
-                color = Color.Blue
-            )
-        }
-    }
-}
-
-@Composable
 fun DrawOne(modifier: Modifier = Modifier) {
-    DrawNumber(
-        pathNumber = Numbers.Nine,
-        modifier = modifier
-    )
-//    Canvas(modifier = modifier) {
-//        val canvasWidth = size.width
-//        val path = Path().apply {
-//            moveTo(0.425414364640884f * canvasWidth, 0.113259668508287f * canvasWidth)
-//            cubicTo(
-//                0.425414364640884f * canvasWidth,
-//                0.113259668508287f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                0.113259668508287f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                0.113259668508287f * canvasWidth
-//            )
-//            cubicTo(
-//                0.577348066298343f * canvasWidth,
-//                0.113259668508287f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth
-//            )
-//            cubicTo(
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth
-//            )
-//            cubicTo(
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth,
-//                0.577348066298343f * canvasWidth,
-//                1f * canvasWidth
-//            )
-//        }
-//        drawPath(path = path, color = Color.Black, style = Stroke(width = 3.dp.value * density))
-//    }
+//    DrawNumber(
+//        pathNumber = Numbers.Nine,
+//        modifier = modifier
+//    )
 }
 
 @Composable
-fun DrawNumber(pathNumber: PathNumber, modifier: Modifier = Modifier) {
+fun DrawNumber(pathNumber: PathNumber, transition: Transition<PathNumber>, modifier: Modifier = Modifier) {
     DrawNumber(
         moveCommand = pathNumber.moveCommand,
         cubicCommand1 = pathNumber.cubicCommand1,
         cubicCommand2 = pathNumber.cubicCommand2,
         cubicCommand3 = pathNumber.cubicCommand3,
         cubicCommand4 = pathNumber.cubicCommand4,
+        transition = transition,
         modifier = modifier
     )
+}
+
+@Composable
+fun DrawNumberDWqdq(
+    modifier: Modifier = Modifier
+) {
+
 }
 
 @Composable
@@ -294,43 +255,128 @@ fun DrawNumber(
     cubicCommand2: List<Float>,
     cubicCommand3: List<Float>,
     cubicCommand4: List<Float>,
+    transition: Transition<PathNumber>,
     modifier: Modifier = Modifier
 ) {
+
+    val moveCommandX by transition.animateFloat {
+        moveCommand.first
+    }
+    val moveCommandY by transition.animateFloat {
+        moveCommand.second
+    }
+
+    val cubicCommand11X by transition.animateFloat {
+        cubicCommand1[0]
+    }
+    val cubicCommand11Y by transition.animateFloat {
+        cubicCommand1[1]
+    }
+    val cubicCommand12X by transition.animateFloat {
+        cubicCommand1[2]
+    }
+    val cubicCommand12Y by transition.animateFloat {
+        cubicCommand1[3]
+    }
+    val cubicCommand13X by transition.animateFloat {
+        cubicCommand1[4]
+    }
+    val cubicCommand13Y by transition.animateFloat {
+        cubicCommand1[5]
+    }
+
+    val cubicCommand21X by transition.animateFloat {
+        cubicCommand2[0]
+    }
+    val cubicCommand21Y by transition.animateFloat {
+        cubicCommand2[1]
+    }
+    val cubicCommand22X by transition.animateFloat {
+        cubicCommand2[2]
+    }
+    val cubicCommand22Y by transition.animateFloat {
+        cubicCommand2[3]
+    }
+    val cubicCommand23X by transition.animateFloat {
+        cubicCommand2[4]
+    }
+    val cubicCommand23Y by transition.animateFloat {
+        cubicCommand2[5]
+    }
+
+    val cubicCommand31X by transition.animateFloat {
+        cubicCommand3[0]
+    }
+    val cubicCommand31Y by transition.animateFloat {
+        cubicCommand3[1]
+    }
+    val cubicCommand32X by transition.animateFloat {
+        cubicCommand3[2]
+    }
+    val cubicCommand32Y by transition.animateFloat {
+        cubicCommand3[3]
+    }
+    val cubicCommand33X by transition.animateFloat {
+        cubicCommand3[4]
+    }
+    val cubicCommand33Y by transition.animateFloat {
+        cubicCommand3[5]
+    }
+
+    val cubicCommand41X by transition.animateFloat {
+        cubicCommand4[0]
+    }
+    val cubicCommand41Y by transition.animateFloat {
+        cubicCommand4[1]
+    }
+    val cubicCommand42X by transition.animateFloat {
+        cubicCommand4[2]
+    }
+    val cubicCommand42Y by transition.animateFloat {
+        cubicCommand4[3]
+    }
+    val cubicCommand43X by transition.animateFloat {
+        cubicCommand4[4]
+    }
+    val cubicCommand43Y by transition.animateFloat {
+        cubicCommand4[5]
+    }
+
     Canvas(modifier = modifier) {
         val canvasWidth = size.width
         val path = Path().apply {
-            moveTo(moveCommand.first * canvasWidth, moveCommand.second * canvasWidth)
+            moveTo(moveCommandX * canvasWidth, moveCommandY * canvasWidth)
             cubicTo(
-                cubicCommand1[0] * canvasWidth,
-                cubicCommand1[1] * canvasWidth,
-                cubicCommand1[2] * canvasWidth,
-                cubicCommand1[3] * canvasWidth,
-                cubicCommand1[4] * canvasWidth,
-                cubicCommand1[5] * canvasWidth
+                cubicCommand11X * canvasWidth,
+                cubicCommand11Y * canvasWidth,
+                cubicCommand12X * canvasWidth,
+                cubicCommand12Y * canvasWidth,
+                cubicCommand13X * canvasWidth,
+                cubicCommand13Y * canvasWidth
             )
             cubicTo(
-                cubicCommand2[0] * canvasWidth,
-                cubicCommand2[1] * canvasWidth,
-                cubicCommand2[2] * canvasWidth,
-                cubicCommand2[3] * canvasWidth,
-                cubicCommand2[4] * canvasWidth,
-                cubicCommand2[5] * canvasWidth
+                cubicCommand21X * canvasWidth,
+                cubicCommand21Y * canvasWidth,
+                cubicCommand22X * canvasWidth,
+                cubicCommand22Y * canvasWidth,
+                cubicCommand23X * canvasWidth,
+                cubicCommand23Y * canvasWidth
             )
             cubicTo(
-                cubicCommand3[0] * canvasWidth,
-                cubicCommand3[1] * canvasWidth,
-                cubicCommand3[2] * canvasWidth,
-                cubicCommand3[3] * canvasWidth,
-                cubicCommand3[4] * canvasWidth,
-                cubicCommand3[5] * canvasWidth
+                cubicCommand31X * canvasWidth,
+                cubicCommand31Y * canvasWidth,
+                cubicCommand32X * canvasWidth,
+                cubicCommand32Y * canvasWidth,
+                cubicCommand33X * canvasWidth,
+                cubicCommand33Y * canvasWidth
             )
             cubicTo(
-                cubicCommand4[0] * canvasWidth,
-                cubicCommand4[1] * canvasWidth,
-                cubicCommand4[2] * canvasWidth,
-                cubicCommand4[3] * canvasWidth,
-                cubicCommand4[4] * canvasWidth,
-                cubicCommand4[5] * canvasWidth
+                cubicCommand41X * canvasWidth,
+                cubicCommand41Y * canvasWidth,
+                cubicCommand42X * canvasWidth,
+                cubicCommand42Y * canvasWidth,
+                cubicCommand43X * canvasWidth,
+                cubicCommand43Y * canvasWidth
             )
         }
         drawPath(path = path, color = Color.Black, style = Stroke(width = 3.dp.value * density))
@@ -398,8 +444,7 @@ fun Gesture() {
 //                            velocity.y
 //                        )
                         // The animation stops when it reaches the bounds.
-                        "current screen size : ${size.width}, ${size.height}".hozLog()
-                        "current offset : ${offset.value.x}, ${offset.value.y}".hozLog()
+//                        "current offset : ${offset.value.x}, ${offset.value.y}".hozLog()
                         offset.updateBounds(
                             lowerBound = Offset(0f, 0f),
                             upperBound = Offset(
@@ -459,16 +504,6 @@ fun Circle(modifier: Modifier = Modifier) {
 
 private fun Offset.toIntOffset() = IntOffset(x.roundToInt(), y.roundToInt())
 
-@Composable
-fun FunComposable() {
-
-}
-
-fun Modifier.random2(
-    onMouvement: (Offset) -> Unit
-): Modifier = Modifier.pointerInput(Unit) {
-
-}
 
 fun Modifier.random(
 
--- app/src/main/java/com/example/androiddevchallenge/ui/theme/PathNumber.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/PathNumber.kt
@@ -6,4 +6,40 @@ data class PathNumber(
     val cubicCommand2: List<Float>,
     val cubicCommand3: List<Float>,
     val cubicCommand4: List<Float>
-)
+) {
+
+    operator fun inc(): PathNumber = when(this) {
+        Numbers.Zero -> {
+            Numbers.One
+        }
+        Numbers.One -> {
+            Numbers.Two
+        }
+        Numbers.Two -> {
+            Numbers.Three
+        }
+        Numbers.Three -> {
+            Numbers.Four
+        }
+        Numbers.Four -> {
+            Numbers.Five
+        }
+        Numbers.Five -> {
+            Numbers.Six
+        }
+        Numbers.Six -> {
+            Numbers.Seven
+        }
+        Numbers.Seven -> {
+            Numbers.Eight
+        }
+        Numbers.Eight -> {
+            Numbers.Nine
+        }
+        Numbers.Nine -> {
+            Numbers.Zero
+        }
+        else -> Numbers.Zero
+    }
+
+}
